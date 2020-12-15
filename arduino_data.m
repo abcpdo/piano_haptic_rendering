a81c77ce27883a8d6828b2e8e12b003d7c9e202d
fanzhenDing <jenniferding0825@gmail.com>
2020-12-14 21:18:23 -0500

    Update arduino_data.m

--- arduino_data.m
+++ arduino_data.m
@@ -7,40 +7,60 @@ classdef arduino_data < handle
     status_sol int
     status_lra int
     pwmSol int
+    p double
+    T double
     y int
     q queue
+    a arduino
+    s serial
 	pwmPin1 int  % motor
 	dirPin1 int 
 	pwmPin2 int  % solenoid
 	dirPin2 int  
 	aPinFSR int  % FSR connected to A0 port 
+	rh double
+	rp double
+	rs double
+	xh double
    end
-   % get arduino
-   a = arduino('/dev/cu.usbserial-AL03G1P2','uno')
+
 	configurePin(a,pwmPin2,'DigitalOutput');
 	configurePin(a,dirPin2,'DigitalOutput');
    methods
-   	function obj = arduino_data(pwmSol,F_motor_out)
+   	function obj = arduino_data(pwmSol,motor_torque)
+	   % get arduino
+  	 	a = arduino('/dev/cu.usbserial-AL03G1P2','uno')
+		s = serial('/dev/cu.usbserial-AL03G1P2','uno')
 		obj.p = pwmSol;
-		obj.F = F_motor_out;
+		obj.T = F_motor_out;
         obj.pwmPin1 = 5;
         obj.dirPin1 = 8;
         obj.pwmPin2 = 6; % solenoid
         obj.dirPin2 = 7; 
         obj.aPinFSR = 0; % FSR connected to A0 port 
+	rh = 0.8654;	//[m]
+    	rp = 0.0486;   //[m]
+    	rs = 0.7443;   //[m]
+	
+	fopen(s);
     end
     function fsr_reading = force_sensor()
-          %function return force on the key sensor
-		fsr_reading = readAnalogPin(aPinFSR);
-  		%Serial.print("Analog reading = ");
-  		%Serial.println(fsrReading);
+         %function return force on the key sensor
+	fsr_reading = readAnalogPin(aPinFSR);
     end
-    function solenoid_status(obj) % 225 - on, 0 - off
-          % function to turn arduino solenoid on/off
-		writeDigitalPin(a,dirPin2,obj.p)
-		delay(3000);
-		analogWrite(pwmPin,0); %  off posiion, only run once when key is pressed
-    end
+    function xh = handle_position(obj)
+	% get position from serial
+	xh = fscanf(s);
+	end
+    function solenoidOn(obj) % 225 - on, 0 - off
+          % function to turn solenoid on
+		writeDigitalPin(a,dirPin2,obj.p);
+    end	
+    	function solenoidOff(obj)
+	% function to turn solenoid off
+    		writeDigitalPin(a,dirPin2,0);
+	end
+
 %      function status_lra = LRA_status(y)
 %          % function to turn arduino LRA  on/off
 %          if (y == 1)
@@ -49,59 +69,10 @@ classdef arduino_data < handle
 %              %LRA off
 %          end
 %      	end
-    function motor_force(obj)
+    function motorTorque(obj)
           % function to control the motor
-          force_m = obj.F;
-          Tp = (force_m*rh*rp)/rs;
-          
-          if(force_m > 0) {
-            digitalWrite(dirPin, HIGH);
-          } else {
-            digitalWrite(dirPin, LOW);
-          }
-      %Compute the duty cycle required to generate Tp (torque at the motor pulley)
-          duty = sqrt(abs(Tp)/0.0183);
-      %Make sure the duty cycle is between 0 and 100%
-          if (duty > 1) {
-          duty = 1;
-          } else if (duty < 0) {
-          duty = 0;
-          }
-          output = (int)(duty* 255); // convert duty cycle to output signal
-          analogWrite(pwmPin,output); // output the signal
-          }
-          
-          void setPwmFrequency(int pin, int divisor) {
-               byte mode;
-          if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
-            switch(divisor) {
-               case 1: mode = 0x01; break;
-               case 8: mode = 0x02; break;
-               case 64: mode = 0x03; break;
-               case 256: mode = 0x04; break;
-               case 1024: mode = 0x05; break;
-               default: return;
-            }
-          if(pin == 5 || pin == 6) {
-               TCCR0B = TCCR0B & 0b11111000 | mode;
-            } else {
-               TCCR1B = TCCR1B & 0b11111000 | mode;
-            }
-            } else if(pin == 3 || pin == 11) {
-               switch(divisor) {
-               case 1: mode = 0x01; break;
-               case 8: mode = 0x02; break;
-               case 32: mode = 0x03; break;
-               case 64: mode = 0x04; break;
-               case 128: mode = 0x05; break;
-               case 256: mode = 0x06; break;
-               case 1024: mode = 0x7; break;
-               default: return;
-            }
-            TCCR2B = TCCR2B & 0b11111000 | mode;
-            }
-               end
-      end
+	  % send force to serial
+	  write(s,obj.T,"double");
    end
 end
 
